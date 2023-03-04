4744b9c52c9c6326736fd4f6816abc4932f9a4a8
andryushchenkoka <kirill2goo@gmail.com>
2023-03-04 23:53:12 +0300

    Добавлены before классы

--- src/test/java/com.tricentis.demowebshop/tests/BaseTest.java
+++ src/test/java/com.tricentis.demowebshop/tests/BaseTest.java
@@ -1,7 +1,12 @@
 package com.tricentis.demowebshop.tests;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.tricentis.demowebshop.helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -10,6 +15,21 @@ import java.util.Date;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class BaseTest {
+
+    @BeforeAll
+    public static void beforeAll() {
+
+        Configuration.baseUrl = "https://demowebshop.tricentis.com";
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+        Configuration.timeout = 20000;
+    }
+
+    @BeforeEach
+    public void BeforeEach() {
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
 
     @AfterEach
     public void afterEach() {
